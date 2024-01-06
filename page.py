5757c826ec9be3a4bf45ded4d80e56faaacc4075
Ari Bermeki <90009669+AriBermeki@users.noreply.github.com>
2024-01-06 15:31:16 +0100

    Update page.py

--- page.py
+++ page.py
@@ -1,29 +1,20 @@
 from inspect import signature 
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
-from typing import Optional, Callable
+from typing import Optional, Callable, Dict, Any, List, Set, Tuple
 from .client.react import generate_react_component
 from .client.utils import create_html_file
 
-
-
-
-
-
-
-
-
-
-class Page(BaseModel):  
+class Page(BaseModel):
     name: str
     url: str
     auth_needed: Optional[str] = None
-    language:str
+    language: str
     builder: Optional[Callable] = None
+    registered_pages: Set[Tuple[str, str]] = set()
 
     class Config:
         arbitrary_types_allowed = True
-        
 
     def as_list(self, param='', all_params=None):
         def call_builder():
@@ -34,13 +25,22 @@ class Page(BaseModel):
                 return self.builder(param)
             else:
                 return self.builder()
-            
-        # Generiere die React-Komponente
-        react_component_html = generate_react_component(self.name, call_builder(), path=self.url, language=self.language)
-        create_html_file('ddd.html', html_content=react_component_html)
-        #print(react_component_html)
-        # Verwende HTMLResponse, um den HTML-Code als Antwort zu senden (
-        return HTMLResponse(content=react_component_html)
+
+        page_info = (self.name, self.url)
+
+        if page_info not in self.registered_pages:
+            react_component_html = generate_react_component(self.name, call_builder(), path=self.url, language=self.language, registered_pages=self.registered_pages)
+            create_html_file('ddd.html', html_content=react_component_html)
+            self.registered_pages.add(page_info)
+            return HTMLResponse(content=react_component_html)
+        else:
+            self.registered_pages.remove(page_info)
+            self.registered_pages.add(page_info)
+            print(self.registered_pages)
+            react_component_html = generate_react_component(self.name, call_builder(), path=self.url, language=self.language, registered_pages=self.registered_pages)
+            create_html_file('ddd.html', html_content=react_component_html)
+            return HTMLResponse(content=react_component_html)
+
         #return {
             #'content':call_builder()
         #}
